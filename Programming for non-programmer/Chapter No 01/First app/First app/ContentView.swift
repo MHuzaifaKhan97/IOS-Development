6d821c598ca02b182abc20f881abe87b91bf902f
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-03-02 23:12:41 +0500

    Chapter no 01: First App

---
+++ Programming for non-programmer/Chapter No 01/First app/First app/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  First app
+//
+//  Created by Muhammad Huzaifa Khan on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!!")
+            .padding()
+     }
+}
+struct ContentView2: View {
+    var body: some View {
+        Text("From Huzaifa Khan").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+//        ContentView2()
+    }
+}
---
+++ Programming for non-programmer/Chapter No 01/First app/First app/First_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_appApp.swift
+//  First app
+//
+//  Created by Muhammad Huzaifa Khan on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct First_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
