a6bc8a91612443040e1a5ff22588d341aec64de9
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-03-12 16:51:11 +0500

    Course 02) Chapter 01

---
+++ Development IOS Essentials Training/Chapter 01 XCode Fundamentals/First Project/First Project/First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_ProjectApp.swift
+//  First Project
+//
+//  Created by Muhammad Huzaifa Khan on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
